148226490fecc26b926346e7ab6c94bfc5d72329
azhark <azharazvtp@gmail.com>
2026-03-29 15:59:01 +0530

    Adding UI

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
-1. git clone https://github.com/Azhar-k/walleto-deploy.git
-2. sudo snap install docker
-3. cd walleto-deploy/
-4. source set-env.sh
-5. docker login ghcr.io -u azhar-k
-6. sudo docker-compose up -d
+1. `git clone https://github.com/Azhar-k/walleto-deploy.git`
+2. `sudo snap install docker`
+3. `cd walleto-deploy/`
+4. Update `set-env.sh` and set `HOST_IP` to your server's public IP address.
+5. `source set-env.sh`
+6. `docker login ghcr.io -u azhar-k`
+7. Ensure port `80` (for UI), `8071` (for API Gateway), `8080`, `8073`, and `5432` are open depending on your needs.
+8. `sudo docker-compose up -d`
--- set-env.sh
+++ set-env.sh
@@ -14,6 +14,12 @@ export POSTGRES_PASSWORD="ChangeMeInProduction123"
 # Example generation: `openssl rand -base64 64`
 export JWT_SECRET=$(openssl rand -base64 64)
 
+# 3. UI API Configuration
+# Public Host IP for the UI to connect to the APIs
+export HOST_IP="http://143.244.139.120"
+export CORE_API_URL="${HOST_IP}:8071/api/core/"
+export USER_API_URL="${HOST_IP}:8071/api/userservice/"
+
 # Output the current status
 echo "----------------------------------------------------"
 echo "Environment variables set:"
@@ -21,6 +27,9 @@ echo "GITHUB_REPOSITORY_LOWERCASE = $GITHUB_REPOSITORY_LOWERCASE"
 echo "POSTGRES_USER                = $POSTGRES_USER"
 echo "POSTGRES_PASSWORD            = [HIDDEN]"
 echo "JWT_SECRET                   = [HIDDEN]"
+echo "HOST_IP                      = $HOST_IP"
+echo "CORE_API_URL                 = $CORE_API_URL"
+echo "USER_API_URL                 = $USER_API_URL"
 echo "----------------------------------------------------"
 echo ""
 echo "You can now run:"
